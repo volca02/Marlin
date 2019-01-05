2e838538b79333fcd80de6e64e2131d3cf5167a3
Filip Volejnik <f.volejnik@gmail.com>
2019-01-05 20:03:23 +0100

    Slightly faster/more aggresive Z movement. Probe is actually 35mm from the nozzle. Corrected.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -650,7 +650,7 @@
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4[, E5]]]]]
  */
 // TODO(Volca): Might want to revisit these.
-#define DEFAULT_MAX_FEEDRATE          { 120, 120, 5, 35 }
+#define DEFAULT_MAX_FEEDRATE          { 120, 120, 8, 35 }
 
 /**
  * Default Max Acceleration (change/s) change = mm/s
@@ -805,7 +805,7 @@
  *    (0,0)
  */
 // TODO(Volca): Measure these!
-#define X_PROBE_OFFSET_FROM_EXTRUDER 37  // X offset: -left  +right  [of the nozzle]
+#define X_PROBE_OFFSET_FROM_EXTRUDER 35  // X offset: -left  +right  [of the nozzle]
 #define Y_PROBE_OFFSET_FROM_EXTRUDER 0   // Y offset: -front +behind [the nozzle]
 #define Z_PROBE_OFFSET_FROM_EXTRUDER -0.2   // Z offset: -below +above  [the nozzle]
 
@@ -841,8 +841,8 @@
  *     But: `M851 Z+1` with a CLEARANCE of 2  =>  2mm from bed to nozzle.
  */
 #define Z_CLEARANCE_DEPLOY_PROBE   8 // Z Clearance for Deploy/Stow
-#define Z_CLEARANCE_BETWEEN_PROBES  5 // Z Clearance between probe points
-#define Z_CLEARANCE_MULTI_PROBE     4 // Z Clearance between multiple probes
+#define Z_CLEARANCE_BETWEEN_PROBES  4 // Z Clearance between probe points
+#define Z_CLEARANCE_MULTI_PROBE     3 // Z Clearance between multiple probes
 // #define Z_AFTER_PROBING             5 // Z position after probing is done
 
 #define Z_PROBE_LOW_POINT          -1 // Farthest distance below the trigger-point to go before stopping
