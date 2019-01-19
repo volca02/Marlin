c664a403ecae29e49f1865a42fc20476b479efc1
Filip Volejnik <f.volejnik@gmail.com>
2019-01-19 23:45:11 +0100

    PID constants for v6 clone and mk52 clone

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -400,9 +400,14 @@
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
 
   // Ultimaker
-  #define DEFAULT_Kp 22.2
-  #define DEFAULT_Ki 1.08
-  #define DEFAULT_Kd 114
+  // #define DEFAULT_Kp 22.2
+  // #define DEFAULT_Ki 1.08
+  // #define DEFAULT_Kd 114
+
+  // E3D V6 Clone. Measured via M303
+  #define DEFAULT_Kp 17.15
+  #define DEFAULT_Ki 1.16
+  #define DEFAULT_Kd 63.56
 
   // MakerGear
   //#define DEFAULT_Kp 7.0
@@ -451,15 +456,21 @@
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define DEFAULT_bedKp 10.00
-  #define DEFAULT_bedKi .023
-  #define DEFAULT_bedKd 305.4
+
+  //#define DEFAULT_bedKp 10.00
+  //#define DEFAULT_bedKi .023
+  //#define DEFAULT_bedKd 305.4
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from pidautotune
   //#define DEFAULT_bedKp 97.1
   //#define DEFAULT_bedKi 1.41
   //#define DEFAULT_bedKd 1675.16
+
+  // MK52 12V clone. Measured with M303
+  #define DEFAULT_bedKp 117.50
+  #define DEFAULT_bedKi 5.14
+  #define DEFAULT_bedKd 671.39
 
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
 #endif // PIDTEMPBED
