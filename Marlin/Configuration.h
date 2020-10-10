0693008e772a4f5203a07ddbd3a11fcc0f62e679
Filip Volejnik <f.volejnik@gmail.com>
2020-10-10 10:10:12 +0200

    botched merge fixed

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -533,23 +533,24 @@
   //#define MIN_BED_POWER 0
   //#define PID_BED_DEBUG // Sends debug data to the serial port.
 
-==== BASE ====
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define DEFAULT_bedKp 10.00
-  #define DEFAULT_bedKi .023
-  #define DEFAULT_bedKd 305.4
-==== BASE ====
+  //#define DEFAULT_bedKp 10.00
+  //#define DEFAULT_bedKi .023
+  //#define DEFAULT_bedKd 305.4
 
-==== BASE ====
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from pidautotune
   //#define DEFAULT_bedKp 97.1
   //#define DEFAULT_bedKi 1.41
   //#define DEFAULT_bedKd 1675.16
 
-==== BASE ====
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
+  // MK52 12V clone. Measured with M303
+  #define DEFAULT_bedKp 117.50
+  #define DEFAULT_bedKi 5.14
+  #define DEFAULT_bedKd 671.39
+
 #endif // PIDTEMPBED
 
 #if EITHER(PIDTEMP, PIDTEMPBED)
