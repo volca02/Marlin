2ebb5e4269abb8257ee3387c5697befa0f110d6f
Filip Volejnik <f.volejnik@gmail.com>
2020-10-10 09:55:53 +0200

    Changes to the ABL offset

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1009,7 +1009,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 35, 0, -0.2 }
+#define NOZZLE_TO_PROBE_OFFSET { 35, 0, -2.0 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
